2abe74c3879cbf4b3a0165af221ae3be78d1dcf5
Serhat Özdemir <serhat.oezd@web.de>
2026-08-19 22:31:58 +0200

    Complete coffee machine OOP project

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ menu = Menu()
 is_on = True
 
 while is_on:
-    choice = input(f"What would you like? ({menu.get_items()})")
+    choice = input(f"What would you like? ({menu.get_items()})").lower()
     if choice == "report":
         maker.report()
         money.report()
+    elif choice == "off":
+        is_on = False
     elif choice in menu.get_items():
         drink = menu.find_drink(choice)
         if maker.is_resource_sufficient(drink):
-            user_money = money.process_coins()
-            if money.make_payment(user_money):
+            if money.make_payment(drink.cost):
                 maker.make_coffee(drink)
 
